47d6869776d751f194a15171656e6ba16c2579d3
codiy <mail@codiy.net>
2022-01-23 15:12:35 +0000

    Fix Some Problems

--- run.sh
+++ run.sh
@@ -3,7 +3,10 @@
 if ! [ -x "$(command -v make)" ]; then
     if ! [ -x "$(command -v docker-compose)" ]; then
         docker build -t shsm
-        docker run -v ${PWD}:/root --rm -it shsm bash --login
+        docker run -v ${PWD}:/root \
+            -v ${HOME}/.gnupg/shsm.sec.key:/root/.gnupg/secret.key \
+            -v /var/run/docker.sock:/var/run/docker.sock \
+            --rm -it shsm bash --login
     else
         docker-compose run --rm shsm bash --login
     fi
